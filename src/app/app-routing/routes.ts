740cedd31440bda80cee31236484f48176ef0bf5
eason <yul196@pitt.edu>
2018-03-26 21:44:22 -0400

    single page app part one

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,5 +10,6 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'menu', component: MenuComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
